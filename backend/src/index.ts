4258ec76077949df61e2b792d45f7ec89bc08446
gautamSH20 <gs841400@gmail.com>
2025-02-25 22:18:24 +0530

    basic backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,24 +2,41 @@ import WebSocket, { WebSocketServer } from "ws";
 
 const ws = new WebSocketServer({ port: 3000 });
 
-let allSocket: WebSocket[] = [];
+interface Users {
+  socket: WebSocket;
+  room: string;
+}
+
+let allSocket: Users[] = [];
 
 ws.on("connection", (socket) => {
-  allSocket.push(socket);
-
   console.log("UserConnected #");
-  const yourSocket = socket;
-  socket.on("message", (message) => {
-    console.log("message recived " + message.toString());
 
-    allSocket.forEach((items) => {
-      const s = items;
-      s.send(message.toString() + "from user");
-    });
+  socket.on("message", (message) => {
+    //@ts-ignore
+    const parseMessage = JSON.parse(message);
+
+    if (parseMessage.type === "join") {
+      allSocket.push({
+        socket,
+        room: parseMessage.payload.roomId,
+      });
+      console.log(allSocket.length);
+    }
+
+    if (parseMessage.type === "chat") {
+      const currentRoom = allSocket.find((x) => x.socket === socket)?.room;
+      console.log("sent message");
+      for (let i = 0; i < allSocket.length; i++) {
+        if (allSocket[i].room === currentRoom) {
+          allSocket[i].socket.send(parseMessage.payload.message);
+        }
+      }
+    }
   });
   socket.on("close", () => {
-    console.log("user disconected ");
-    allSocket = allSocket.filter((x) => x != socket);
+    console.log("user disconnected");
+    allSocket = allSocket.filter((x) => x.socket != socket);
     console.log(allSocket.length);
   });
 });
