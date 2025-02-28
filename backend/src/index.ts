1a2a0f3c7d45ac8fd3e8988dc5a34dae00794a1e
gautamSH20 <gs841400@gmail.com>
2025-02-28 12:59:36 +0530

    backend done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,31 +12,35 @@ let allSocket: Users[] = [];
 ws.on("connection", (socket) => {
   console.log("UserConnected #");
 
-  socket.on("message", (message) => {
-    //@ts-ignore
-    const parseMessage = JSON.parse(message);
+  try {
+    socket.on("message", (message) => {
+      //@ts-ignore
+      const parseMessage = JSON.parse(message);
+      console.log("working");
+      if (parseMessage.type === "join") {
+        allSocket.push({
+          socket,
+          room: parseMessage.payload.roomId,
+        });
+        console.log(allSocket.length);
+      }
 
-    if (parseMessage.type === "join") {
-      allSocket.push({
-        socket,
-        room: parseMessage.payload.roomId,
-      });
-      console.log(allSocket.length);
-    }
-
-    if (parseMessage.type === "chat") {
-      const currentRoom = allSocket.find((x) => x.socket === socket)?.room;
-      console.log("sent message");
-      for (let i = 0; i < allSocket.length; i++) {
-        if (allSocket[i].room === currentRoom) {
-          allSocket[i].socket.send(parseMessage.payload.message);
+      if (parseMessage.type === "chat") {
+        const currentRoom = allSocket.find((x) => x.socket === socket)?.room;
+        console.log("sent message");
+        for (let i = 0; i < allSocket.length; i++) {
+          if (allSocket[i].room === currentRoom) {
+            allSocket[i].socket.send(parseMessage.payload.message);
+          }
         }
       }
-    }
-  });
-  socket.on("close", () => {
-    console.log("user disconnected");
-    allSocket = allSocket.filter((x) => x.socket != socket);
-    console.log(allSocket.length);
-  });
+    });
+    socket.on("close", () => {
+      console.log("user disconnected");
+      allSocket = allSocket.filter((x) => x.socket != socket);
+      console.log(allSocket.length);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
